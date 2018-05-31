66ba14cf023759a65becd8f5a5fbd3488e64f9cc
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-05-31 07:03:37 +0700

    setting up application server and configuration

--- config.py
+++ config.py
@@ -1,1 +1,20 @@
-asdasd
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    THREADS_PER_PAGE = 2
+    CSRF_ENABLED     = True
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://faizghifari:F41zGH01!@localhost/smart_hospital_test'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://administrator:admin@localhost/smart_hospital'
+
+config_type = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
+
+
--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-asdasd
+import os
+
+from app import create_app
+
+config_name = os.getenv('FLASK_ENV')
+app = create_app(config_name)
+
+if __name__ == '__main__':
+    app.run()
