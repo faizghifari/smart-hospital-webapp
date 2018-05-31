53bb2c9ddc9eaf8c635634a3b878265c9ad8e5d0
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-05-31 15:04:48 +0700

    add index blueprint for dashboard

--- app/index/routes.py
+++ app/index/routes.py
@@ -1,6 +1,12 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, render_template
+from flask_login import login_required, current_user
 from . import index
 
 @index.route('/')
 def home():
     return redirect(url_for('auth.login'))
+
+@index.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('index/dashboard.html', title='Dashboard')
