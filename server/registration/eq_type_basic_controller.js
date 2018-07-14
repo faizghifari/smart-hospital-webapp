98936c81cb4588f3c44801f87e445990d55183ff
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-07-15 00:20:41 +0800

    add reg api for facilities

--- server/registration/eq_type_basic_controller.js
+++ server/registration/eq_type_basic_controller.js
@@ -1,5 +1,6 @@
 const medical_equipments_type_basic = require('../models').medical_equipments_type_basic;
 const type_proc = require('./eq_type_procedure_controller');
+const facilities = require('./facilities_controller');
 
 module.exports = {
     create(type_id, data) {
@@ -13,6 +14,10 @@ module.exports = {
         .then(type_basic => {
             data.procedures.forEach(proc => {
                 type_proc.create_basic(type_basic.id, proc.procedure);
+            });
+
+            data.facilities.forEach(facility => {
+                facilities.create_basic(type_basic.id, facility.facility);
             });
         })
         .catch(error => console.log(error));
--- server/registration/eq_type_high_controller.js
+++ server/registration/eq_type_high_controller.js
@@ -1,5 +1,6 @@
 const medical_equipments_type_high = require('../models').medical_equipments_type_high;
 const type_proc = require('./eq_type_procedure_controller');
+const facilities = require('./facilities_controller');
 
 module.exports = {
     create(type_id,data) {
@@ -14,6 +15,10 @@ module.exports = {
             data.procedures.forEach(proc => {
                 type_proc.create_high(type_high.id, proc.procedure);
             });
+
+            data.facilities.forEach(facility => {
+                facilities.create_high(type_high.id, facility.facility);
+            })
         })
         .catch(error => console.log(error));
     }
