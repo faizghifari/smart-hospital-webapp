b27164b6538fdd6673d0b60cd2b27234eaf8f500
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2018-07-21 21:22:03 +0800

    testing role in equipment api

--- server/registration/routes.js
+++ server/registration/routes.js
@@ -1,8 +1,9 @@
 const equipment = require('./equipment');
+const role_controller = require('../role/role_controller');
 
 module.exports = (app) => {
     app.post('/api/equipment/registration', equipment.create);
-    app.get('/api/equipment', equipment.list);
+    app.get('/api/equipment', role_controller.valid_login, role_controller.role_check, equipment.list);
     app.get('/api/equipment/:equipment_id', equipment.retrieve);
     app.get('/api/equipment/:equipment_id/details', equipment.retrieve_details);
     app.put('/api/equipment/:equipment_id', equipment.update);
