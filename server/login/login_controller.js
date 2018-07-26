b9dab2145d942216560a9dbeb255a199ba349924
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2018-07-26 14:53:37 +0800

    generate pin after login and store it to users table

--- server/login/login_controller.js
+++ server/login/login_controller.js
@@ -9,24 +9,37 @@ module.exports = {
         var user = data.username;
         var password = data.password;
 
-        pin = randomize('0', 6);
-        
         users
         .findOne({
             where: {username: user}
         })
         .then(async result => {
             if (!result) {
-                return res.status(400).send('User not found'); //if user not in DB
+                return res.status(400).send('User not found'); 
             } else if(result) {
-                const matches = await bcrypt.compare(password, result.password_hash); //validate the password
-                if (matches) {
-                    jwtLogin.sign(req, res, user, "topsecret", 1, false);
-                } else {
+                const matches = await bcrypt.compare(password, result.password_hash);
+                if (!matches) {
                     return res.status(400).send('Auth failed. Wrong password.');   
+                } else {
+                    module.exports.update_pin(result.id);
+                    jwtLogin.sign(req, res, user, "topsecret", 1, false);
                 }
             }
         });
+    },
+
+    update_pin(user_id) {
+        let generated_pin = randomize('0', 6);
+        users
+        .findById(user_id)
+        .then(result => {
+            result
+            .update({
+                login_pin: generated_pin
+            })
+            .catch((error) => console.log(error));
+        })
+        .catch(error => console.log(error));
     },
 
     logout(req, res) {
