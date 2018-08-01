96f53cd5b37641dec5a99bb7c1ffcdd6196c04f9
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2018-08-01 09:21:10 +0800

    add register routes

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ require('./server/admin/routes')(app);
 require('./server/registration/routes')(app);
 require('./server/auth/routes')(app);
 require('./server/login/routes')(app);
+require('./server/register/routes')(app);
 
 const productivity = require('./server/monitoring/productivity');
 const safety = require('./server/monitoring/safety');
