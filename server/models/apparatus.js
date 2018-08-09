11c315345ce7364bef8f5ec1a40202e5809ec663
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2018-08-09 12:54:13 +0800

    remove qrcode from apparatus model and migration

--- server/models/apparatus.js
+++ server/models/apparatus.js
@@ -3,7 +3,6 @@ module.exports = (sequelize, DataTypes) => {
         apparatus_name: DataTypes.STRING,
         apparatus_desc: DataTypes.STRING,
         apparatus_sn: DataTypes.STRING,
-        apparatus_qrcode: DataTypes.STRING,
         apparatus_calibration_due_on: DataTypes.DATEONLY
     }, {});
     apparatus.associate = (models) => {
