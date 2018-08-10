aed566c6c7573002cb549bc8ba84b082e14b9f6f
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-08-10 09:39:46 +0800

    add routes, integrate usage with productivity

--- server/usage/usage.js
+++ server/usage/usage.js
@@ -2,29 +2,35 @@ const moment = require('moment');
 
 const medical_equipments_booking_model = require('../models').medical_equipments_booking;
 const booking = require('./booking');
+const productivity = require('../monitoring/productivity');
 
 module.exports = {
     async validate_usage(booking_id) {
         let booking_data = await module.exports.retrieve_booking(booking_id);
         let now = moment();
 
+        let result = false;
         if (moment(now).isBetween(booking_data.start_time, booking_data.end_time)) {
-            return true;
-        } else {
-            return false;
+            result = true;
         }
+        booking_data.result = result;
+
+        return booking_data;
     },
 
     async receive_usage(req,res) {
-        let is_validated = await module.exports.validate_usage(req.params.booking_id);
-        if (is_validated) {
+        let booking_data = await module.exports.validate_usage(req.params.booking_id);
+        if (booking_data.result) {
             let data = {
                 'booking_id': req.params.booking_id,
                 'is_usage': true
             };
 
+            let usage_update = await productivity.receive_usage(booking_data);
             let result = await booking.update_booking(data);
 
+            result.usage = usage_update;
+            
             let result_stringified = JSON.stringify(result);
             return res.status(200).send(result_stringified);
         } else {
