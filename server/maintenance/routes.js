4030973d3bcdabb6a5581f5aad1cb53331267904
Lingga Adi Pratama <linggaadi4nd@gmail.com>
2018-11-15 09:35:49 +0800

    cors routes maintenance

--- server/maintenance/routes.js
+++ server/maintenance/routes.js
@@ -3,8 +3,18 @@ const adverse_reporting = require(  './adverse_event_reporting');
 const corrective =  require(        './corrective');
 const preventive =  require(        './preventive');
 const work_order =  require(        './work_order');
+const cors = require('cors');
 
 module.exports = (app) => {
+
+    
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+      
+      
     app.get('/report/breakdown', reporting.list);
     app.get('/report/breakdown/open', reporting.list_open);
     app.get('/report/breakdown/closed', reporting.list_closed);
