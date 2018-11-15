2da8516849ab36671a189e4f94c37550fe5d7da1
Lingga Adi Pratama <linggaadi4nd@gmail.com>
2018-11-15 09:36:42 +0800

    cors routes login

--- server/login/routes.js
+++ server/login/routes.js
@@ -2,11 +2,14 @@ const login = require('../login/login_controller');
 const cors = require('cors');
 module.exports = (app) => {
     //cors
-    app.use(cors())
 
-    app.post('/login', login.login,function(req,res,next){
-        res.json({msg:'This is CORS-enabled for all origins!'})
-    });
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+      
+    app.post('/login', login.login);
     app.post('/login/verify', login.verify);
     app.get('/logout', login.logout);    
 };
