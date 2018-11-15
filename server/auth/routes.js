7e5853f10c501021f73fcfdf37c3f13b0a505abe
Lingga Adi Pratama <linggaadi4nd@gmail.com>
2018-11-15 09:39:08 +0800

    cors routes auth

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -1,5 +1,12 @@
 const auth_controller = require('./auth_controller');
+const cors = require('cors');
 
 module.exports = (app) => {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     app.post('/api/auth/get_token', auth_controller.getToken);    
 };
